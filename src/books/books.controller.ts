e8ed747bf87b2798e9cec04331ef82c00307dea2
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-03-02 17:15:38 +0100

    Added module with endpoints for books

---
+++ src/books/books.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  ParseUUIDPipe,
+  NotFoundException,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDTO } from './dto/create-book.dto';
+import { UpdateBookDTO } from './dto/update-book.dto';
+
+@Controller('books')
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.booksService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const book = await this.booksService.getById(id);
+    if (!book) throw new NotFoundException('Book not found');
+    return book;
+  }
+
+  @Post('/')
+  create(@Body() bookData: CreateBookDTO) {
+    return this.booksService.create(bookData);
+  }
+
+  @Put('/:id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() bookData: UpdateBookDTO,
+  ) {
+    if (!(await this.booksService.getById(id)))
+      throw new NotFoundException('Book not found');
+
+    await this.booksService.updateById(id, bookData);
+    return { success: true };
+  }
+
+  @Delete('/:id')
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.booksService.getById(id)))
+      throw new NotFoundException('Book not found');
+    await this.booksService.deleteById(id);
+    return { success: true };
+  }
+}
