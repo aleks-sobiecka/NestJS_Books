bef51228f0e6e8e0b70fcb8b41a8cfe7393f6187
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-03-05 17:43:55 +0100

    Added new admin-auth guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import {
   Controller,
   Get,
+  Delete,
   Param,
   ParseUUIDPipe,
   NotFoundException,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -21,5 +25,15 @@ export class UsersController {
     const user = await this.usersService.getById(id);
     if (!user) throw new NotFoundException('User not found');
     return user;
+  }
+
+  @Delete(':id')
+  @UseGuards(AdminAuthGuard)
+  @UseGuards(JwtAuthGuard)
+  public async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.usersService.getById(id)))
+      throw new NotFoundException('User not found');
+    await this.usersService.deleteById(id);
+    return { success: true };
   }
 }
